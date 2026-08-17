09a871bc4baa955281b043367e1d06c9e764f948
Bruce Kang <brucekangcn@gmail.com>
2026-08-17 16:51:48 +0800

    improve docstring of ROI_RATIO in config.py

--- src/yolo_detect_server/config.py
+++ src/yolo_detect_server/config.py
@@ -1,10 +1,11 @@
 ROI_RATIO = 0.4
-"""Search region Ratio of packing locations.
+"""
+Search region ratio of packing locations.
 
-   if the center point of detection is in this region, then the object is
-   considered to be at the corresponding location. The region is placed at the
-   center of the region of the location, with width and height scaled using this
-   ratio.
+if the center point of detection is in this region, then the object is
+considered to be at the corresponding location. The region is placed at the
+center of the region of the location, with width and height scaled using this
+ratio.
 """
 
 MODEL_PATH = "weights/best.pt"
