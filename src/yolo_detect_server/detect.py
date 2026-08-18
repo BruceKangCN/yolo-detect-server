08526302c2871f329b54c533239d1e0a4c4ffd92
Bruce Kang <brucekangcn@gmail.com>
2026-08-18 08:26:52 +0800

    feat: support sort by priority, fix format and lint problems

--- src/yolo_detect_server/detect.py
+++ src/yolo_detect_server/detect.py
@@ -5,7 +5,6 @@ from ultralytics import YOLO
 from .config import MODEL_PATH
 from .util import xywh_to_xyxy
 
-
 model = YOLO(MODEL_PATH)
 
 df = pl.read_csv("regions.csv")
@@ -13,17 +12,18 @@ REGIONS = [xywh_to_xyxy(row) for row in df.iter_rows()]
 
 
 def find_packed(img: np.ndarray) -> list[str]:
-    result = model.predict(img, conf=0.6)[0] # type: ignore
+    result = model.predict(img, conf=0.6)[0]  # type: ignore
 
     packed = []
     # TODO: 理论上存在误检导致相同库位中存在多个结果的可能性，后续可以考虑进行优化
-    for box in result.boxes: # type: ignore
-        if box.cls.cpu().numpy()[0] != 0: # type: ignore
+    for box in result.boxes:  # type: ignore
+        if box.cls.cpu().numpy()[0] != 0:  # type: ignore
             continue
-        x, y, _, _ = box.xyxyn.cpu().numpy()[0] # type: ignore
+        x, y, _, _ = box.xyxyn.cpu().numpy()[0]  # type: ignore
         for region in REGIONS:
             if region.contains(x, y):
-                packed.append(region.name)
+                packed.append({"name": region.name, "priority": region.priority})
                 break
+    sorted(packed, key=lambda region: region["priority"])
 
-    return packed
+    return [region["name"] for region in packed]
