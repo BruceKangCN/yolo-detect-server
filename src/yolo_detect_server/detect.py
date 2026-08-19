fix: fix position normalization

src/yolo_detect_server/detect.py
@@ -43,12 +43,12 @@ def find_packed(img: np.ndarray) -> list[str]:
     packed = []
     # TODO: 理论上存在误检导致相同库位中存在多个结果的可能性，后续可以考虑进行优化
     for box in boxes:
-        x = (box[0] + box[2]) / orig_w
-        y = (box[1] + box[3]) / orig_h
+        x = (box[0] + box[2]) / 2 / orig_w
+        y = (box[1] + box[3]) / 2 / orig_h
         for region in REGIONS:
             if region.contains(x, y):
                 packed.append({"name": region.name, "priority": region.priority})
                 break
-    sorted(packed, key=lambda region: region["priority"])
+    packed.sort(key=lambda region: region["priority"])
 
     return [region["name"] for region in packed]